2d5ed2a856ac8a4ae6c24d19a8c3003a504fd357
Wayne Lund <wxlund@yahoo.com>
2016-02-17 06:58:52 -0800

    Applied suggestions.  QueueBrowse is working now although there is an issue with passing in the exchange. The default exchange works fine.

--- rabbitmq-jms-boot-cli/src/main/java/io/pivotal/pa/rabbitmq/jms/raw/client/MessageBrowserClient.java
+++ rabbitmq-jms-boot-cli/src/main/java/io/pivotal/pa/rabbitmq/jms/raw/client/MessageBrowserClient.java
@@ -1,6 +1,5 @@
 package io.pivotal.pa.rabbitmq.jms.raw.client;
 
-import java.time.LocalTime;
 import java.util.Enumeration;
 import java.util.Random;
 
@@ -8,11 +7,11 @@ import javax.jms.Message;
 import javax.jms.Queue;
 import javax.jms.QueueBrowser;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.rabbitmq.jms.admin.RMQDestination;
 
@@ -25,21 +24,20 @@ public class MessageBrowserClient implements JMSClientWorker {
 	private static Logger log = LoggerFactory.getLogger(MessageBrowserClient.class);
 
 	@Autowired
-	private Session session;
-	
+	private Session jmsSession;
+
 	@Autowired
-	@Qualifier("QueueBrowser")
 	QueueBrowser queueBrowser;
-	
+
 	@Autowired
 	private AppProperties appProperties;
-	
+
 	@Autowired
 	private JMSProperties jmsProperties;
-	
+
 	@Autowired
 	private Random randy;
-	
+
 	int messageCounter = 0;
 
 	@Override
@@ -50,51 +48,50 @@ public class MessageBrowserClient implements JMSClientWorker {
 		if (appProperties.batchSize > 0) {
 			log.info("Using a batch size of " + appProperties.batchSize + ", turning transactions on.");
 		}
-		if(jmsProperties.jmsPriority>9) {
+		if (jmsProperties.jmsPriority > 9) {
 			log.warn("jmsPriority is set to a number greater than 9 which is not allow by the spec.  Setting to 9.");
 			jmsProperties.jmsPriority = 9;
-		} else if(jmsProperties.jmsPriority > -1) {
-			log.info("Using JMS priority of "+jmsProperties.jmsPriority);
+		} else if (jmsProperties.jmsPriority > -1) {
+			log.info("Using JMS priority of " + jmsProperties.jmsPriority);
 		}
-		if(jmsProperties.jmsReplyTo != null && !"".equals(jmsProperties.jmsReplyTo)) {
-			log.info("Using jmsReplyTo="+jmsProperties.jmsReplyTo);
+		if (jmsProperties.jmsReplyTo != null && !"".equals(jmsProperties.jmsReplyTo)) {
+			log.info("Using jmsReplyTo=" + jmsProperties.jmsReplyTo);
 		}
-		if(appProperties.sendPoisonPercent>0) {
-			if(appProperties.sendPoisonPercent > 100) {
-				log.warn("sendPercent="+appProperties.sendPoisonPercent+" which is higher than 100.  Setting to 100.");
+		if (appProperties.sendPoisonPercent > 0) {
+			if (appProperties.sendPoisonPercent > 100) {
+				log.warn("sendPercent=" + appProperties.sendPoisonPercent
+						+ " which is higher than 100.  Setting to 100.");
 				appProperties.sendPoisonPercent = 100;
 			}
-			log.info("Sending poison messages enabled.  Poison=\""+appProperties.poisonMessage+"\" sendPercent="+appProperties.sendPoisonPercent+"%");
+			log.info("Sending poison messages enabled.  Poison=\"" + appProperties.poisonMessage + "\" sendPercent="
+					+ appProperties.sendPoisonPercent + "%");
 		}
 	}
-	
-	//TODO: make sure this method is reentrant
+
+	// TODO: make sure this method is reentrant
 	public void start() throws Exception {
 
-		if (session != null) {
-			String queueName = ((RMQDestination)queueBrowser.getQueue()).getDestinationName();
-			System.out.println("Sending " + appProperties.numMessages + " messages with a delay of " + appProperties.delay + " and payload \""
-					+ appProperties.messageStr + "\" to destination " + queueName);
+		if (jmsSession != null) {
+			String queueName = ((RMQDestination) queueBrowser.getQueue()).getQueueName();
+			System.out.println(
+					"Browsing " + appProperties.numMessages + " messages with a delay of " + appProperties.delay
+							+ " and payload \"" + appProperties.messageStr + "\" to destination " + queueName);
 
 			try {
-				for (messageCounter = 0; messageCounter < appProperties.numMessages; messageCounter++) {
-					
-					if(appProperties.showCounter) {
-						System.out.println(LocalTime.now()+"> Sending message [" + messageCounter + "]");
+				Queue queue = queueBrowser.getQueue();
+				Enumeration msgs = queueBrowser.getEnumeration();
+
+				if (!msgs.hasMoreElements()) {
+					System.out.println("No messages in queue");
+				} else {
+					while (msgs.hasMoreElements()) {
+						TextMessage tempMsg = (TextMessage) msgs.nextElement();
+						System.out.println("Browsing Message: " + tempMsg.getText());
 					}
-					Queue queue = queueBrowser.getQueue();
-					Enumeration msgs = queueBrowser.getEnumeration();
-					
-					if (!msgs.hasMoreElements() ) {
-						System.out.println("No messages in queue");
-					} else {
-						while (msgs.hasMoreElements()) { 
-					        Message tempMsg = (Message)msgs.nextElement(); 
-					        System.out.println("Message: " + tempMsg); 
-					    }
-					}
-					
-					if(appProperties.delay > 0) { Thread.sleep(appProperties.delay); }
+				}
+
+				if (appProperties.delay > 0) {
+					Thread.sleep(appProperties.delay);
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
